06d73fae30425bef2d1b4d2efa5b4d26c7a35b72
Abdelrhmansalah99 <116437632+Abdelrhmansalah99@users.noreply.github.com>
2022-12-13 19:45:54 +0200

    Add files via upload

---
+++ DARWISHController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcsams66.Controllers
+{
+    public class DARWISHController : Controller
+    {
+        // GET: DARWISH
+        public ActionResult Index()
+        {
+            AdventureWorks2017Entities dc = new AdventureWorks2017Entities();
+            var item = dc.Employees.ToList();  
+
+            return View(item);
+        }
+    }
+}
